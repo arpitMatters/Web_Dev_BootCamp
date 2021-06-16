ebdd71e63aca6ca572864664d7e4fad15b0bb456
arpitMatters <43750177+arpitMatters@users.noreply.github.com>
2021-06-17 02:07:26 +0530

    Add files via upload

---
+++ REACT/Keeper App 2/src/notes.js
@@ -0,0 +1,33 @@
+const notes = [
+  {
+    key: 1,
+    title: "Delegation",
+    content:
+      "Q. How many Swimmers does it take to change a Tube Light? A. None – It’s a hardware problem"
+  },
+  {
+    key: 2,
+    title: "Loops",
+    content:
+      "How to keep a Dancers in the shower forever. Show him the shampoo bottle instructions: Lather. Rinse. Repeat."
+  },
+  {
+    key: 3,
+    title: "Arrays",
+    content:
+      "Q. Why did the Singers quit his job? A. Because he didn't get arrays."
+  },
+  {
+    key: 4,
+    title: "Hardware vs. Software",
+    content:
+      "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software."
+  },
+  {
+    key: 5,
+    title: "Big ideas",
+    content: "Eat more sushi"
+  }
+];
+
+export default notes;
